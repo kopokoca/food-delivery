b0c0360140376c6e95313f0ef493c567252d0fa9
kopokoca <golikovlad@yandex.ru>
2020-05-07 00:32:09 +0300

    authorization added

---
+++ js/main.js
@@ -0,0 +1,91 @@
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
+}
+
+// day 1
+
+const buttonAuth = document.querySelector('.button-auth');
+const modalAuth = document.querySelector('.modal-auth');
+const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+
+let login = localStorage.getItem('delivery-food');
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle('is-open');
+}
+
+
+function autorized() {
+  // Функция выхода с аккаунта
+  function logOut() {
+    login = '';
+    localStorage.removeItem('delivery-food');
+    buttonAuth.style.display = '';
+    userName.style.display = '';
+    buttonOut.style.display = '';
+    buttonOut.removeEventListener('click', logOut);
+    checkAuth();
+  }
+  console.log('Авторизован');
+
+  userName.textContent = login;
+
+  buttonAuth.style.display = 'none';
+  userName.style.display = 'inline';
+  buttonOut.style.display = 'block';
+
+  // При нажатии на "Выйти" выполняем функцию
+  buttonOut.addEventListener('click', logOut);
+}
+
+function notAuthorized() {
+  console.log('Не авторизован');
+
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    // Проверяем, ввёл ли пользователь логин
+    if (login != '') {
+      localStorage.setItem('delivery-food', login);
+
+      toggleModalAuth();
+      loginInput.style.outline = "none";
+      loginInput.placeholder = "";
+      buttonAuth.removeEventListener('click', toggleModalAuth);
+      closeAuth.removeEventListener('click', toggleModalAuth);
+      logInForm.removeEventListener('submit', logIn);
+      logInForm.reset();
+      checkAuth();
+    } else {
+      // Если пользователь не ввёл имя
+      loginInput.style.outline = "1px solid red";
+      loginInput.placeholder = "Введите логин";
+    }  
+  }
+
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit', logIn);
+}
+
+function checkAuth () {
+  if(login) {
+    autorized();
+  } else {
+    notAuthorized();
+  }
+}
+
+checkAuth();
